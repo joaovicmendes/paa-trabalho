494654766a8dc8a8eadc45bced07227a66109ed1
João Victor Mendes <mendesjoaovic@gmail.com>
2019-11-22 20:47:45 -0300

    Trabalho 3 adicionado

---
+++ Trabalho 3/main.cpp
@@ -0,0 +1,75 @@
+/*
+    Universidade Federal de São Carlos – UFSCar
+    Departamento de Computação
+
+    1001525 - Projeto e Análise de Algoritmos – Trabalho 3
+    Prof. Dr. Murilo Coelho Naldi
+
+    Aluno: João Victor Mendes Freire
+    RA: 758943
+*/
+
+#include <iostream>
+
+#define MAX(x,y) ((x) > (y) ? (x) : (y))
+
+typedef struct packet 
+{
+    int    weight; // Packet weight
+    int ornaments; // Numeber of ornaments in packet
+} Packet;
+
+int mochila(Packet* p, int num_packets, int max_weight);
+
+int main(int argc, char **argv)
+{
+    int num_branches, num_packets, max_weight;
+    int o, w;
+    Packet *packets;
+
+    scanf("%d ", &num_branches);
+    for (int i = 0; i < num_branches; i++)
+    {
+        scanf("%d ", &num_packets);
+        scanf("%d ", &max_weight);
+
+        packets = (Packet *)malloc(sizeof(Packet) * (num_packets + 1));
+
+        for (int j = 1; j <= num_packets; j++)
+        {
+            scanf("%d %d ", &o, &w);
+            packets[j].weight    = w;
+            packets[j].ornaments = o;
+        }
+
+        std::cout << "Galho " << i + 1 << ":\n";
+        std::cout << "Numero total de enfeites: " << mochila(packets, num_packets, max_weight) << "\n\n";
+    } 
+
+    return 0;
+}
+
+int mochila(Packet* p, int num_packets, int max_weight)
+{
+    int **A;
+    
+    A = (int**)malloc(sizeof(int *) * (num_packets + 1));
+    for (int i = 0; i <= num_packets; i++)
+        A[i] = (int*)malloc(sizeof(int) * (max_weight + 1));
+
+    for (int j = 0; j < max_weight; j++)
+        A[0][j] = 0;
+
+    for (int i = 1; i <= num_packets; i++)
+    {
+        for (int j = 0; j <= max_weight; j++)
+        {
+            if (j < p[i].weight)
+                A[i][j] = A[i - 1][j];
+            else
+                A[i][j] = MAX(A[i-1][j], A[i-1][j-p[i].weight] + p[i].ornaments);
+        }
+    }
+
+    return A[num_packets][max_weight];
+}
